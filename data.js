0faca088727ec7e088d10a8d95865a7158b4d5ab
fnohr0608-create <fnohr.0608@gmail.com>
2025-12-29 12:20:15 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+const entries = {
+  "2025-08-01": {
+    video: "https://youtu.be/8OzMvzjnVPY",
+    text: "Ein leiser Anfang. Viel Luft, wenig Worte."
+  },
+  "2025-08-03": {
+    video: "VIDEO_ID_HIER",
+    text: "Müde, aber klarer als gedacht."
+  }
+};
