eb7ef2887c0fdff748e49644610f540a30d228c7
fnohr0608-create <fnohr.0608@gmail.com>
2025-12-29 12:27:43 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const entries = {
   "2025-08-01": {
-    video: "https://youtu.be/8OzMvzjnVPY",
+    video: "8OzMvzjnVPY",
     text: "Ein leiser Anfang. Viel Luft, wenig Worte."
   },
   "2025-08-03": {
