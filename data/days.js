1ac2aad1ffe80527ce45254e7a55e10b32d00784
fnohr0608-create <fnohr.0608@gmail.com>
2025-12-30 23:15:11 +0100

    Update days.js

--- data/days.js
+++ data/days.js
@@ -4,7 +4,7 @@
 const days = {
   "2025-08-21": {
     video: "https://www.youtube-nocookie.com/embed/8OzMvzjnVPY",
-    description: "Heute war viel los in meinem Kopf."
+    text: "Heute war viel los in meinem Kopf."
   }
 };
 
